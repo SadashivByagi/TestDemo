12a50e7e476babeb87c2dc49c53da88dfa161a2c
Sadashiv Byagi_ons <43012335+SadashivByagi@users.noreply.github.com>
2023-09-21 12:58:59 +0100

    FirstCommit

---
+++ JavaTestDedmo/src/com/test/project/Testing.java
@@ -0,0 +1,12 @@
+package com.test.project;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing Has been passed");
+
+	}
+
+}
